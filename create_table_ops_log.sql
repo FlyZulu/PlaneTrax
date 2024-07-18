ee53527e896beffadf77827479e8c01b7b52740e
FlyZulu <davidson.mw100@gmail.com>
2024-07-18 14:38:40 -0500

    Add files via upload

--- create_table_ops_log.sql
+++ create_table_ops_log.sql
@@ -1,6 +1,3 @@
-CREATE DATABASE adsb;
-
-USE adsb;
 
 CREATE TABLE `ops_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -16,36 +13,5 @@ CREATE TABLE `ops_log` (
   `speed` varchar(10) DEFAULT NULL,
   `vert_rate` varchar(10) DEFAULT NULL,
   `notes` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
-
-
-
-CREATE TABLE `ops_log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `message_date` DATETIME DEFAULT NULL,
-  `now` varchar(100) DEFAULT NULL,
-  `hex` varchar(100) DEFAULT NULL,
-  `flight` varchar(100) DEFAULT NULL,
-  `runway` varchar(3) DEFAULT NULL,
-  `op` varchar(1) DEFAULT NULL,
-  `tg` varchar(1) DEFAULT NULL,
-  `distance` varchar(100) DEFAULT NULL,
-  `altitude` varchar(100) DEFAULT NULL,
-  `lat` varchar(100) DEFAULT NULL,
-  `lon` varchar(100) DEFAULT NULL,
-  `track` varchar(100) DEFAULT NULL,
-  `speed` varchar(100) DEFAULT NULL,
-  `vert_rate` varchar(100) DEFAULT NULL,
-  `seen_pos` varchar(100) DEFAULT NULL,
-  `seen` varchar(100) DEFAULT NULL,
-  `rssi` varchar(100) DEFAULT NULL,
-  `messages` varchar(100) DEFAULT NULL,
-  `category` varchar(100) DEFAULT NULL,
-  `squawk` varchar(100) DEFAULT NULL,
-  `nucp` varchar(100) DEFAULT NULL,
-  `mlat` varchar(100) DEFAULT NULL,
-  `tisb` varchar(100) DEFAULT NULL,
-  `rec_msg_sec` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
